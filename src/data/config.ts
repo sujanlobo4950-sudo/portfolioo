4ccad5dac745c732aa566e501d4dc2bea6fe37fb
sujanlobo4950-sudo <sujanlobo.4950@gmail.com>
2026-05-11 23:21:05 +0400

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  title: "Naresh Khatri | Full-Stack Developer",
+  title: "Sujan Lobo | IT and Marketing Specialist",
   description: {
     long: "Explore the portfolio of Naresh, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
     short:
