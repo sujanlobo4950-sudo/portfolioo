5890d10cee948de171911468d2e14f1652ee723e
sujanlobo4950-sudo <sujanlobo.4950@gmail.com>
2026-05-11 23:45:28 +0400

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,14 +1,14 @@
 const config = {
   title: "Sujan Lobo | IT and Marketing Specialist",
   description: {
-    long: "Explore the portfolio of Naresh, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
+    long: "Dynamic and results-driven IT & Digital Marketing Professional with experience in CRM management, digital marketing, IT administration, and sales operations support. Skilled in managing end-to-end lead generation campaigns, CRM systems, social media marketing, and real estate operational processes. Experienced in handling IT infrastructure, troubleshooting technical issues, maintaining databases, and supporting business productivity through technology solutions.Proficient in Adobe Photoshop, Canva, CorelDRAW, HTML/CSS, SQL, Java, C/C++, Android Studio, and Salesforce Marketing Cloud. Strong background in graphic design, customer relationship management, VAT filing, contract preparation (Forms A, B, F & MOU), and social media management.Able to combine technical expertise, creative marketing strategies, and operational management to improve workflow efficiency, strengthen brand presence, and support business growth",
     short:
-      "Discover the portfolio of Naresh, a full-stack developer creating interactive web experiences and innovative projects.",
+      "IT & Digital Marketing professional with experience in CRM management, lead generation, sales support, and IT administration. Skilled in managing digital marketing campaigns, designing promotional content, handling social media platforms, and maintaining IT infrastructure. Experienced in real estate operations, VAT filing, contract documentation, and database management, with a strong focus on improving business efficiency and workflow automation.",
   },
   keywords: [
-    "Naresh",
+    "Sujan",
     "portfolio",
-    "full-stack developer",
+    "IT and Marketing Specialist",
     "creative technologist",
     "web development",
     "3D animations",
@@ -23,23 +23,23 @@ const config = {
     "Spline",
     "Framer Motion",
   ],
-  author: "Naresh Khatri",
-  email: "naresh.khatri2345@gmail.com",
-  site: "https://nareshkhatri.site",
+  author: "Sujan Lobo",
+  email: "sujusuja0404@gmail.com",
+  site: "https://www.linkedin.com/in/sujan-lobo-366abb2a0",
 
   // for github stars button
-  githubUsername: "naresh-khatri",
-  githubRepo: "3d-portfolio",
+  githubUsername: "sujanlobo4950-sudo",
+  githubRepo: "portfolioo",
 
   get ogImg() {
     return this.site + "/assets/seo/og-image.png";
   },
   social: {
-    twitter: "https://x.com/nothotchaddi",
-    linkedin: "https://www.linkedin.com/in/naresh-khatri/",
-    instagram: "https://www.instagram.com/hotchaddi",
-    facebook: "https://www.facebook.com/HotChaddi/",
-    github: "https://github.com/Naresh-Khatri",
+    twitter: "https://x.com",
+    linkedin: "https://www.linkedin.com/in/sujan-lobo-366abb2a0",
+    instagram: "https://www.instagram.com/sujanlobo.nk/",
+    facebook: "https://www.facebook.com",
+    github: "https://github.com/sujanlobo4950-sudo",
   },
 };
 export { config };
