c2242a365b8a6527911dd1f68dfe9a316c8ba9dd
sujanlobo4950-sudo <sujanlobo.4950@gmail.com>
2026-05-12 09:55:34 +0400

    Update hero.tsx

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -76,14 +76,14 @@ const HeroSection = () => {
                       "cursor-default font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text "
                     )}
                   >
-                    A Full Stack Web Developer
+                    A Digital Marketing & IT Specialist
                   </p>
                 </BlurIn>
               </div>
               <div className="mt-8 flex flex-col gap-3 w-fit">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1MTSsUA8V7Po2AsNXT8kZ5sLOpzC8l7qm/view?usp=sharing"
+                    "https://drive.google.com/file/d/1ssz9BxwkD-DfSaGgfT4GEFQoU-DkpUWC/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
